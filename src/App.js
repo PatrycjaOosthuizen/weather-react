e9c03fd865927bacb95c43853804b6ab99c2318e
Patrycja Oosthuizen <pielszym@googlemail.com>
2024-05-29 06:34:38 +0100

    Added skeleton of the project

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from "react";
+import "./Weather.css";
 import Weather from "./Weather";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>React Weather app</h1>
-        <Weather city="Tokyo" />
-      </header>
+      <Weather />
     </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -1,16 +1,78 @@
 import React from "react";
-import axios from "axios";
-import { Puff } from "react-loader-spinner";
 
-export default function Weather(props) {
-  function handleResponse(response) {
-    alert(
-      `The weather in ${response.data.name} is ${response.data.main.temp}°C`
-    );
-  }
-  let apiKey = "2a2eaa51d996796495bf456e5b58adf4";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
-
-  axios.get(apiUrl).then(handleResponse);
-  return <Puff color="#00BFFF" height={100} width={100} timeout={3000} />;
+export default function Weather() {
+  let weatherData = {
+    city: "London",
+    date: "Tuesday 07:15",
+    description: "Cloudy",
+    temperature: 5,
+    humidity: 80,
+    wind: 30,
+  };
+  return (
+    <div className="Weather weather-app">
+      <header>
+        <form className="search-form">
+          <input
+            type="search"
+            placeholder="Enter a city.."
+            required
+            className="search-form-input"
+            autocomplete="on"
+          />
+          <input type="submit" value="Search" className="search-form-button" />
+        </form>
+      </header>
+      <main>
+        <div className="weather-app-data">
+          <div>
+            <h1 className="weather-app-city">{weatherData.city}</h1>
+            <p className="weather-app-details">
+              <span>Last updated: {weatherData.date}</span>,{" "}
+              {weatherData.description} <span></span>
+              <br />
+              Humidity: <strong>{weatherData.humidity} %</strong>, Wind:
+              <strong> {weatherData.wind} km/h</strong>
+            </p>
+          </div>
+          <div className="weather-app-temperature-container">
+            <div>☁️</div>
+            <div className="weather-app-temperature">
+              {weatherData.temperature}
+            </div>
+            <div className="weather-app-unit">°C</div>
+          </div>
+        </div>
+        <div className="weather-forecast"></div>
+      </main>
+      <footer>
+        <p>
+          This project was coded by {""}
+          <a
+            href="https://www.shecodes.io/graduates/104913-patrycja-oosthuizen"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Patrycja Oosthuizen
+          </a>
+          , is {""}
+          <a
+            href="https://github.com/PatrycjaOosthuizen/weather-react"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on Github {""}
+          </a>
+          and hosted on
+          <a
+            href="https://react-weather-app-po.netlify.app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {""} Netlify
+          </a>
+        </p>
+      </footer>
+    </div>
+  );
 }
