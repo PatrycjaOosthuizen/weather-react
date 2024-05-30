62aeb964c8d76323b675b5e4b0f8cb2ed3093b7c
Patrycja Oosthuizen <pielszym@googlemail.com>
2024-05-30 05:29:31 +0100

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <Weather />
+      <div className="container">
+        <Weather />
+      </div>
     </div>
   );
 }
